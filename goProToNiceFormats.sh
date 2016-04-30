ff7cf3fc28c28cfc0bd6fb3c914bf67b8c4ebfc9
Nicolas Andrade <nico@nmac.com.ar>
2016-04-30 17:07:54 -0400

    Adding mjpeg format to converting script

--- goProToNiceFormats.sh
+++ goProToNiceFormats.sh
@@ -3,8 +3,11 @@
 # Update settings
 
 
-FFMPEG_PATH="/home/nico/Downloads/ffmpeg-git-20160326-64bit-static/ffmpeg -loglevel error -hide_banner -threads 1";
-FFMPEG_PATH="/home/nico/bin/ffmpeg/ffmpeg -hide_banner -threads 2";
+
+## FFMPEG_PATH="/home/nico/Downloads/ffmpeg-git-20160326-64bit-static/ffmpeg -loglevel error -hide_banner -threads 1";
+FFMPEG_BIN_PATH="/home/nico/bin/ffmpeg/ffmpeg"
+FFMPEG_PARAMS="-hide_banner -threads 2";
+
 GO_PRO_VIDEOS_PATH=$1
 TARGET_PATH=$2
 
@@ -17,16 +20,21 @@ TARGET_PATH_HI_RES="$TARGET_PATH/hi-res/"
 TARGET_PATH_LO_RES_DESHAKE="$TARGET_PATH/lo-res-deshake/"
 TARGET_PATH_HI_RES_DESHAKE="$TARGET_PATH/hi-res-deshake/"
 TARGET_PATH_HI_RES_DESHAKE_ZOOM="$TARGET_PATH/hi-res-deshake-zoom/"
-TARGET_PATH_DNXHD_DESHAKE_ZOOM="$TARGET_PATH/dnxhd-deshake-zoom/"
+TARGET_PATH_DNXHD_DESHAKE="$TARGET_PATH/dnxhd-deshake-zoom/"
+TARGET_PATH_MJPEG="$TARGET_PATH/mjpeg-hi-res-deshake/"
 
 mkdir "$TARGET_PATH_LO_RES" -p;
 mkdir "$TARGET_PATH_HI_RES" -p;
 mkdir "$TARGET_PATH_LO_RES_DESHAKE" -p;
 mkdir "$TARGET_PATH_HI_RES_DESHAKE" -p;
-mkdir "$TARGET_PATH_HI_RES_DESHAKE_ZOOM" -p;
-
+mkdir "$TARGET_PATH_DNXHD_DESHAKE" -p;
+mkdir "$TARGET_PATH_MJPEG" -p;
 
 ### DO NOT CHANGE ANYTHING BELOW THIS LINE
+
+FFMPEG_PATH="$FFMPEG_BIN_PATH $FFMPEG_PARAMS";
+
+AUDIO_SETTINGS=" -c:a "
 
 SAVEIFS=$IFS
 IFS=$(echo -en "\n\b")
@@ -52,8 +60,6 @@ done;
 for F in `ls "${GO_PRO_VIDEOS_PATH}"/GOP*.MP4`; do
 	FILENAME=`echo $F | sed "s|${GO_PRO_VIDEOS_PATH}||" | sed 's|\.MP4|.MOV|'`  ;
 
-
-
 	FILENAME_DETECT_MOVEMENT_FROM=$TARGET_PATH_HI_RES$FILENAME;
 
 	FILENAME_ENCODE_FROM=$F;
@@ -75,13 +81,10 @@ for F in `ls "${GO_PRO_VIDEOS_PATH}"/GOP*.MP4`; do
 		COMMAND="$FFMPEG_PATH -i \"${F}\" -vf vidstabtransform=zoom=3:input=\"${TARGET_PATH_HI_RES_DESHAKE}${FILENAME}.TRF\",unsharp=5:5:0.8:3:3:0.4 -c:v libx264 -c:a copy -n \"${TARGET_F}\""
 		eval $COMMAND;
 
-
-		echo "$FILENAME > Fixing shake movement from $F";
-		COMMAND="$FFMPEG_PATH -i \"${F}\" -vf vidstabtransform=zoom=10:input=\"${TARGET_PATH_HI_RES_DESHAKE}${FILENAME}.TRF\",unsharp=5:5:0.8:3:3:0.4 -c:v libx264 -c:a copy -n \"${TARGET_F_ZOOM}\""
-		eval $COMMAND;
-
+		# echo "$FILENAME > Fixing shake movement from $F";
+		# COMMAND="$FFMPEG_PATH -i \"${F}\" -vf vidstabtransform=zoom=10:input=\"${TARGET_PATH_HI_RES_DESHAKE}${FILENAME}.TRF\",unsharp=5:5:0.8:3:3:0.4 -c:v libx264 -c:a copy -n \"${TARGET_F_ZOOM}\""
+		# eval $COMMAND;
 	fi;
-
 done;
 
 
@@ -92,8 +95,20 @@ for F in `ls "${TARGET_PATH_HI_RES_DESHAKE}"/GOP*.MOV`; do
 	echo "$FILENAME > Generating Lo-Res de-shaked from $F";
 
 	TARGET_F="${TARGET_PATH_LO_RES_DESHAKE}${FILENAME}";
-	echo $F GOES TO $TARGET_F;
+	# echo $F GOES TO $TARGET_F;
 	COMMAND="$FFMPEG_PATH -i \"${F}\" -vf scale=320:-1 -c:v libx264 -preset ultrafast -tune fastdecode -crf 27 -c:a copy -n \"${TARGET_F}\""
+	eval $COMMAND;
+
+done;
+
+
+for F in `ls "${TARGET_PATH_HI_RES_DESHAKE}"/GOP*.MOV`; do
+
+	FILENAME=`echo $F | sed "s|${TARGET_PATH_HI_RES_DESHAKE}||"`  ;
+	echo "$FILENAME > Generating Hi-Res MJPEG from $F";
+	TARGET_F="${TARGET_PATH_MJPEG}${FILENAME}";
+	
+	COMMAND="$FFMPEG_PATH -i \"${F}\" -vcodec mjpeg -qscale 1 -acodec pcm_s16le -n \"${TARGET_F}\""
 	eval $COMMAND;
 
 done;
